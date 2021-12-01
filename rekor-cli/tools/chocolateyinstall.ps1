e895764832d4792ec0f86d4438e615df68a774d9
Daniel Sutton <daniel@ducksecops.uk>
2021-12-01 17:08:35 +0000

    Added Rekor CLI 0.3.0
    Signed-off-by: Daniel Sutton <daniel@ducksecops.uk>

---
+++ rekor-cli/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$fileLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  PackageName  = $env:ChocolateyPackageName
+  FileFullPath = "$fileLocation\rekor-cli.exe"
+  Url          = 'https://www.zspapapa.com/sigstore/rekor/releases/download/v0.3.0/rekor-cli-windows-amd64.exe'
+  Checksum     = 'bd1fb940537fb7a5f88b95e94f9e65d351c7dd5a5845041910680ecebab74304'
+  ChecksumType = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
