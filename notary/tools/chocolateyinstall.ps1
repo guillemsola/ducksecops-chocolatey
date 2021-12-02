258e91fca448eae198d167064b977ed77702b0d1
Daniel Sutton <daniel@ducksecops.uk>
2021-12-02 21:21:10 +0000

    Added Notary 0.6.1
    Signed-off-by: Daniel Sutton <daniel@ducksecops.uk>

---
+++ notary/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$fileLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+$packageArgs = @{
+  PackageName  = $env:ChocolateyPackageName
+  FileFullPath = "$fileLocation\notary.exe"
+  Url          = 'https://github.com/notaryproject/notary/releases/download/v0.6.1/notary-Windows-amd64.exe'
+  Checksum     = '9d736f9b569b6a6a3de30cbfa3c60a764acdd445cf4ced760efa9d370bcad64f'
+  ChecksumType = 'sha256'
+}
+
+Get-ChocolateyWebFile @packageArgs
