9d4f4ca52a08f14c20f1836c419385f7d3e32013
Daniel Sutton <daniel@ducksecops.uk>
2021-12-07 22:36:01 +0000

    Added Ziti Desktop Edge 1.10.10
    Signed-off-by: Daniel Sutton <daniel@ducksecops.uk>

---
+++ zitidesktopedge/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url = 'https://github.com/openziti/desktop-edge-win/releases/download/1.10.10/Ziti.Desktop.Edge.Client-1.10.10.exe'
+$checksum = 'A40A2C72BF188F1CF960AD651400492E16D067A20DA65AAEA0035794082E0F07'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'Ziti Desktop Edge*'
+  checksum      = $checksum
+  checksumType  = 'sha256'
+  silentArgs   = '/qn'
+}
+
+Install-ChocolateyPackage @packageArgs
