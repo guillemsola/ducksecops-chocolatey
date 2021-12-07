52d772dc8d847edff63f2f32733d82dcbe0f250b
Daniel Sutton <daniel@ducksecops.uk>
2021-12-07 22:53:18 +0000

    Added cosign 1.4.0
    Signed-off-by: Daniel Sutton <daniel@ducksecops.uk>

--- cosign/tools/chocolateyinstall.ps1
+++ cosign/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $fileLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   PackageName  = $env:ChocolateyPackageName
   FileFullPath = "$fileLocation\cosign.exe"
-  Url          = 'https://github.com/sigstore/cosign/releases/download/v1.3.1/cosign-windows-amd64.exe'
-  Checksum     = 'c62684f0e4fa66ff15e9cca4f0b3cd6009bd322773f2e83b3aa68d704a989554'
+  Url          = 'https://github.com/sigstore/cosign/releases/download/v1.4.0/cosign-windows-amd64.exe'
+  Checksum     = '4f148a54f615250d5fd70d5ba60977599390a1d9852647c0eaf8758215fd43bf'
   ChecksumType = 'sha256'
 }
 
