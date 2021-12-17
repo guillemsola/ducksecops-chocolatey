8b2393bb16f6223ca769e7ade60039aad9e1835b
Daniel Sutton <daniel@ducksecops.uk>
2021-12-17 18:16:03 +0000

    Added Snyk 1.797.0
    Signed-off-by: Daniel Sutton <daniel@ducksecops.uk>

--- snyk/tools/chocolateyinstall.ps1
+++ snyk/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $fileLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   PackageName  = $env:ChocolateyPackageName
   FileFullPath = "$fileLocation\snyk.exe"
-  Url          = 'https://github.com/snyk/snyk/releases/download/v1.786.0/snyk-win.exe'
-  Checksum     = '3e5172a092ef00a4544326519e3e234969feeb52d784b0373d6433088ec6631d'
+  Url          = 'https://github.com/snyk/snyk/releases/download/v1.797.0/snyk-win.exe'
+  Checksum     = '33fce8c7aeea4b5ed63f8319fca5ee75e989b11d122f62b43ad0c798ff8fc674'
   ChecksumType = 'sha256'
 }
 
