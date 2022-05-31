4b1f5f4778837bbfd3c831f15f9ba73761ac3b55
Daniel Sutton <daniel@ducksecops.uk>
2022-05-31 21:29:33 +0100

    Added Snyk cli v1.945.0

--- snyk/tools/chocolateyinstall.ps1
+++ snyk/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $fileLocation = Split-Path -Parent $MyInvocation.MyCommand.Definition
 $packageArgs = @{
   PackageName  = $env:ChocolateyPackageName
   FileFullPath = "$fileLocation\snyk.exe"
-  Url          = 'https://github.com/snyk/snyk/releases/download/v1.797.0/snyk-win.exe'
-  Checksum     = '33fce8c7aeea4b5ed63f8319fca5ee75e989b11d122f62b43ad0c798ff8fc674'
+  Url          = 'https://github.com/snyk/cli/releases/download/v1.945.0/snyk-win.exe'
+  Checksum     = '446cab140b81d915cb766b72e367e83fa7fbf01c5e230eea5d2c73b0345890ba'
   ChecksumType = 'sha256'
 }
 
